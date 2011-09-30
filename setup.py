a84bf6f9e35ea86539bb3fcc89d6f3942756ecd9
seb <>
2011-09-30 13:29:54 +0200

    Updated CHANGES.txt and version in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 name = 'OERPLib'
 version = '0.1'
-release = '0.1.2'
+release = '0.2.0'
 description = 'Python library which allows to easily interact with a remote OpenERP server via XML-RPC.'
 author = u"ABF Osiell - Sébastien Alix"
 author_email = 'sebastien.alix@osiell.com'
