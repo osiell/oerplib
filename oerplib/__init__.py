3823b5db399dedee7b6e0df37e2f6a315062c18f
seb <>
2012-03-07 14:17:50 +0100

    Metadata about OERPLib added into the 'oerplib' package (author, email, licence, version and release) + Fix the 'setup.py' to avoid encoding issues.

--- oerplib/__init__.py
+++ oerplib/__init__.py
@@ -6,6 +6,13 @@ an `OpenERP` server. You can use this one to write Python programs that performs
 a variety of automated jobs that communicate with an `OpenERP` server.
 
 """
+
+__author__ = 'ABF Osiell - Sebastien Alix'
+__email__ = 'sebastien.alix@osiell.com'
+__licence__ = 'LGPL v3'
+__version__ = '0.4'
+__release__ = '0.4.0'
+
 from oerplib.oerp import OERP
 #from oerplib.dms import DMS
 from oerplib import rpc, error
