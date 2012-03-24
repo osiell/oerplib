0a7ff26b3ef7956fb3d6b46badda6161766cf634
Sébastien Alix <sebastien.alix@osiell.com>
2012-03-24 12:33:41 +0100

    'example/example.py' updated

--- examples/example.py
+++ examples/example.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
+"""A sample script to demonstrate some of functionalities of OERPLib."""
 import oerplib
 
-# XMLRPC server configuration (NETRPC is not supported)
-XMLRPC_SERVER = 'localhost'
-XMLRPC_SERVER_PORT = 8069
-# DMS service configuration
-FTP_SERVER = 'localhost'
-FTP_SERVER_PORT = 8021
+# XMLRPC server configuration (NETRPC is also supported)
+SERVER = 'localhost'
+PROTOCOL = 'xmlrpc'
+PORT = 8069
 # Name of the OpenERP database to use
-DATABASE = 'database-dev'
-# Use OpenERP Document Management System module
-# (this one must be installed on the server side)
-USE_DMS = True
+DATABASE = 'db_name'
 
 USER = 'admin'
 PASSWORD = 'password'
@@ -19,9 +15,10 @@ PASSWORD = 'password'
 try:
     # Login
     oerp = oerplib.OERP(
-            server=XMLRPC_SERVER,
-            port=XMLRPC_SERVER_PORT,
+            server=SERVER,
             database=DATABASE,
+            protocol=PROTOCOL,
+            port=PORT,
             )
     oerp.login(USER, PASSWORD)
 
@@ -39,28 +36,32 @@ try:
     oerp.execute('res.users', 'write', user_ids[0], {'name': u"Administrator"})
 
     # Execute - create
-    #FIXME
+    new_user_id = oerp.execute('res.users', 'create', {'login': u"New user"})
 
-    # Get a report
-    pdf_path = oerp.exec_report('sale.order', 'sale.order', 1)
-
-    # --------------------------- #
-    # -- Search IDs of objects -- #
-    # --------------------------- #
+    # ------------------------- #
+    # -- Convenients methods -- #
+    # ------------------------- #
 
     # Search IDs of a model that match criteria
     assert oerp.user.id in oerp.search('res.users', [('name', 'ilike', u"Administrator"),])
 
-    # ---------------------------- #
-    # -- Read data of an object -- #
-    # ---------------------------- #
-    print(oerp.read('res.users', oerp.user.id))
+    # Create a record
+    new_user_id = oerp.create('res.users', {'login': u"new_user"})
+
+    # Read data of a record
+    user_data = oerp.read('res.users', new_user_id)
+
+    # Write a record
+    oerp.write('res.users', [new_user_id], {'name': u"New user"})
+
+    # Delete a record
+    oerp.unlink('res.users', new_user_id)
 
     # -------------------- #
     # -- Browse objects -- #
     # -------------------- #
 
-    # Browse an object 
+    # Browse an object
     user = oerp.browse('res.users', oerp.user.id)
     print(user.name)
     print(user.company_id.name)
@@ -72,54 +73,33 @@ try:
         for line in order.order_line:
             print('\t{0}'.format(line.name))
 
-    # ------------------------------- #
-    # -- OERP Dictionary interface -- #
-    # ------------------------------- #
+    # ----------------------- #
+    # -- Download a report -- #
+    # ----------------------- #
 
-    # An object may be accessed via a dictionary interface
-    print(oerp['res.users'][oerp.user.id].name)
+    so_pdf_path = oerp.report('sale.order', 'sale.order', 1)
+    inv_pdf_path = oerp.report('webkitaccount.invoice', 'account.invoice', 1)
 
-    # Modifications can be saved too
-    oerp['res.users'][oerp.user.id].name = u"James Bond"
-    oerp.write(oerp['res.users'][oerp.user.id])
+    # -------------------- #
+    # -- List databases -- #
+    # -------------------- #
 
-    # Changes can be canceled
-    oerp['res.users'][oerp.user.id].name = u"Thierry la Fronde"
-    oerp.reset(oerp['res.users'][oerp.user.id])
-    print(oerp['res.users'][oerp.user.id].name) # Display "James Bond"
+    # List databases
+    print(oerp.db.list())
 
-    # And they can be updated from Open ERP server
-    oerp.refresh(oerp['res.users'][oerp.user.id])
+    # Create a database in background
+    oerp.db.create(
+            super_admin_passwd='super_admin_passwd',
+            database='my_new_db',
+            demo_data=True, lang='fr_FR',
+            admin_passwd='admin_passwd')
 
-    # Many2One fields can be updated easily (with an ID or with an object)
-    oerp['res.users'][oerp.user.id].address_id = oerp.browse('res.partner.address', 1)
-    oerp.write(oerp['res.users'][oerp.user.id])
-    oerp['res.users'][oerp.user.id].address_id = 1  # ID supplied: same effect as above, implicit relation used
-    oerp.write(oerp['res.users'][oerp.user.id])
-
-    # -------------------------------- #
-    # -- Document Management System -- #
-    # -------------------------------- #
-
-    if USE_DMS:
-        # Get a DMS object
-        dms = oerplib.DMS(FTP_SERVER, FTP_SERVER_PORT)
-        # Login on the DMS
-        dms.login(USER, PASSWORD)
-        print("---- Content of {0}".format(dms.pwd()))
-        dms.dir()
-        dms.cwd(DATABASE)
-        print("---- Content of {0}".format(dms.pwd()))
-        dms.dir()
-        dms.cwd('Documents')
-        print("---- Content of {0}".format(dms.pwd()))
-        dms.dir()
-        dms.cwd('Sales Order')
-        print("---- Content of {0}".format(dms.pwd()))
-        dms.dir()
-
-        dms.cwd('All Sales Order/SO001')
-        pdf_path = dms.get('SO001_print.pdf')
+    # Create a database (process blocked until the end of the operation)
+    oerp.db.create_and_wait(
+            super_admin_passwd='super_admin_passwd',
+            database='my_new_db',
+            demo_data=True, lang='fr_FR',
+            admin_passwd='admin_passwd')
 
 except oerplib.error.Error as e:
     print(e)
