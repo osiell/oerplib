c516b1432bc9a5ba0fb533b6cbc334a9c5698d7e
Sébastien Alix <sebastien.alix@osiell.com>
2013-02-13 10:48:54 +0100

    Unit test udpated

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -13,7 +13,7 @@ from oerplib import tools
 class TestTools(unittest.TestCase):
 
     def test_clean_version_numeric(self):
-        version = tools.clean_version('6.1-test')
+        version = tools.clean_version('6.1')
         self.assertEqual(version, '6.1')
 
     def test_clean_version_alphanumeric(self):
@@ -32,14 +32,15 @@ class TestTools(unittest.TestCase):
         self.assertEqual(tools.v('7.0alpha'), [7, 0])
 
     def test_v_cmp(self):
+        # [(v1, v2, is_inferior), ...]
         versions = [
             ('7.0', '6.1', False), ('6.1', '7.0', True),
             ('7.0alpha', '6.1', False), ('6.1beta', '7.0', True),
             ('6.1beta', '5.0.16', False), ('5.0.16alpha', '6.1', True),
         ]
-        for v1, v2, res in versions:
+        for v1, v2, is_inferior in versions:
             result = tools.v(v1) < tools.v(v2)
-            if res:
+            if is_inferior:
                 self.assertTrue(result)
             else:
                 self.assertFalse(result)
