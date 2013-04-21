01d08af2410b6135fcba2dc1c4b5f9a831785de4
Sébastien Alix <sebastien.alix@osiell.com>
2013-04-21 20:00:36 +0200

    Example script updated

--- examples/example.py
+++ examples/example.py
@@ -15,11 +15,7 @@ PASSWORD = 'password'
 try:
     # Login
     oerp = oerplib.OERP(
-            server=SERVER,
-            database=DATABASE,
-            protocol=PROTOCOL,
-            port=PORT,
-            )
+        server=SERVER, database=DATABASE, protocol=PROTOCOL, port=PORT)
     oerp.login(USER, PASSWORD)
 
     # ----------------------- #
@@ -28,46 +24,41 @@ try:
 
     # Execute - search
     user_ids = oerp.execute('res.users', 'search', [('id', '=', oerp.user.id)])
-
     # Execute - read
     user_data = oerp.execute('res.users', 'read', user_ids[0])
-
     # Execute - write
     oerp.execute('res.users', 'write', user_ids[0], {'name': "Administrator"})
-
     # Execute - create
     new_user_id = oerp.execute('res.users', 'create', {'login': "New user"})
 
-    # ------------------------- #
-    # -- Convenients methods -- #
-    # ------------------------- #
+    # --------------------- #
+    # -- Dynamic methods -- #
+    # --------------------- #
 
+    # Get the model
+    user_obj = oerp.get('res.users')
     # Search IDs of a model that match criteria
-    assert oerp.user.id in oerp.search('res.users', [('name', 'ilike', "Administrator"),])
-
+    user_obj.search([('name', 'ilike', "Administrator")])
     # Create a record
-    new_user_id = oerp.create('res.users', {'login': "new_user"})
-
-    # Read data of a record
-    user_data = oerp.read('res.users', new_user_id)
-
+    new_user_id = user_obj.create({'login': "new_user"})
+    # Read data of a record (just the name field)
+    user_data = user_obj.read([new_user_id], ['name'])
     # Write a record
-    oerp.write('res.users', [new_user_id], {'name': "New user"})
-
+    user_obj.write([new_user_id], {'name': "New user"})
     # Delete a record
-    oerp.unlink('res.users', new_user_id)
+    user_obj.unlink([new_user_id])
 
     # -------------------- #
     # -- Browse objects -- #
     # -------------------- #
 
     # Browse an object
-    user = oerp.browse('res.users', oerp.user.id)
+    user = user_obj.browse(oerp.user.id)
     print(user.name)
     print(user.company_id.name)
-
     # .. or many objects
-    for order in oerp.browse('sale.order', [68, 69]):
+    order_obj = oerp.get('sale.order')
+    for order in order_obj.browse([68, 69]):
         print(order.name)
         print(order.partner_id.name)
         for line in order.order_line:
@@ -80,29 +71,25 @@ try:
     so_pdf_path = oerp.report('sale.order', 'sale.order', 1)
     inv_pdf_path = oerp.report('webkitaccount.invoice', 'account.invoice', 1)
 
-    # -------------------- #
-    # -- List databases -- #
-    # -------------------- #
+    # ------------------------- #
+    # -- Databases management-- #
+    # ------------------------- #
 
     # List databases
     print(oerp.db.list())
-
     # Create a database in background
     oerp.db.create(
-            super_admin_passwd='super_admin_passwd',
-            database='my_new_db',
-            demo_data=True, lang='fr_FR',
-            admin_passwd='admin_passwd')
-
-    # Create a database (process blocked until the end of the operation)
-    oerp.db.create_and_wait(
-            super_admin_passwd='super_admin_passwd',
-            database='my_new_db',
-            demo_data=True, lang='fr_FR',
-            admin_passwd='admin_passwd')
+        'super_admin_passwd', 'my_db',
+        demo_data=True, lang='fr_FR',
+        admin_passwd='admin_passwd')
+    # ... after a while, dump it
+    my_dump = oerp.db.dump('super_admin_password', 'my_db')
+    # Create a new database from the dump
+    oerp.db.restore('super_admin_password', 'my_new_db', my_dump)
+    # Delete the old one
+    oerp.db.drop('super_admin_password', 'my_db')
 
 except oerplib.error.Error as e:
     print(e)
 except Exception as e:
     print(e)
-
