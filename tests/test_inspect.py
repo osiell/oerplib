996b57368c6d85154c33c01bd0bc2622b1776e96
Sébastien Alix <sebastien.alix@osiell.com>
2013-10-30 19:19:17 +0100

    Unit tests updated ('inspect.dependencies()' method)

--- tests/test_inspect.py
+++ tests/test_inspect.py
@@ -19,12 +19,55 @@ class TestInspect(unittest.TestCase):
         self.user = self.oerp.login(ARGS.user, ARGS.passwd, ARGS.database)
 
     def test_relations(self):
-        res = self.oerp.inspect.relations(
+        graph = self.oerp.inspect.relations(
             ['res.users'],
             maxdepth=1,
             whitelist=['*'], blacklist=[],
             attrs_whitelist=['*'], attrs_blacklist=[])
-        self.assertIsInstance(res, oerplib.service.inspect.relations.Relations)
+        self.assertIsInstance(graph, oerplib.service.inspect.relations.Relations)
+
+    def test_dependencies(self):
+        graph = self.oerp.inspect.dependencies()
+        self.assertIsInstance(
+            graph, oerplib.service.inspect.dependencies.Dependencies)
+
+    def test_dependencies_with_module(self):
+        graph = self.oerp.inspect.dependencies(['base'])
+        self.assertIsInstance(
+            graph, oerplib.service.inspect.dependencies.Dependencies)
+
+    def test_dependencies_with_models(self):
+        graph = self.oerp.inspect.dependencies(models=['res.partner*'])
+        self.assertIsInstance(
+            graph, oerplib.service.inspect.dependencies.Dependencies)
+
+    def test_dependencies_with_wrong_module(self):
+        self.assertRaises(
+            oerplib.error.InternalError,
+            self.oerp.inspect.dependencies, ['wrong_module'])
+
+    def test_dependencies_with_module_and_models(self):
+        graph = self.oerp.inspect.dependencies(['base'], ['res.partner*'])
+        self.assertIsInstance(
+            graph, oerplib.service.inspect.dependencies.Dependencies)
+
+    def test_dependencies_with_module_and_models_restricted(self):
+        graph = self.oerp.inspect.dependencies(
+            ['base'], ['res.partner*'], restrict=True)
+        self.assertIsInstance(
+            graph, oerplib.service.inspect.dependencies.Dependencies)
+
+    def test_dependencies_with_module_and_models_blacklist(self):
+        graph = self.oerp.inspect.dependencies(
+            ['base'], ['res.partner*'], ['res.partner.bank'])
+        self.assertIsInstance(
+            graph, oerplib.service.inspect.dependencies.Dependencies)
+
+    def test_dependencies_with_module_and_models_blacklist_restricted(self):
+        graph = self.oerp.inspect.dependencies(
+            ['base'], ['res.partner*'], ['res.partner.bank'], restrict=True)
+        self.assertIsInstance(
+            graph, oerplib.service.inspect.dependencies.Dependencies)
 
     def test_scan_on_change(self):
         res = self.oerp.inspect.scan_on_change(['res.users'])
