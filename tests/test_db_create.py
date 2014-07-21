22517ccf5be978f13e21916a2b73eed200815183
Sébastien Alix <sebastien.alix@osiell.com>
2014-07-21 20:11:14 +0200

    [FIX] Unit tests: use 'create_database' method in 'TestDBCreate.test_db_create' for Odoo 8.0 and superior

--- tests/test_db_create.py
+++ tests/test_db_create.py
@@ -8,6 +8,7 @@ except:
 from args import ARGS
 
 import oerplib
+from oerplib.tools import v
 
 
 class TestDBCreate(unittest.TestCase):
@@ -19,15 +20,26 @@ class TestDBCreate(unittest.TestCase):
 
     def test_db_create(self):
         if ARGS.database not in self.oerp.db.list():
-            res = self.oerp.db.create_and_wait(
-                ARGS.super_admin_passwd,
-                ARGS.database,
-                demo_data=False,
-                lang='en_US',
-                admin_passwd=ARGS.passwd)
-            self.assertIsInstance(res, list)
-            self.assertNotEqual(res, list())
-            self.assertEqual(res[0]['login'], 'admin')
-            self.assertEqual(res[0]['password'], ARGS.passwd)
+            if v(self.oerp.version) >= v('8.0'):
+                res = self.oerp.db.create_database(
+                    ARGS.super_admin_passwd,
+                    ARGS.database,
+                    False,
+                    'en_US',
+                    ARGS.passwd)
+                self.assertTrue(res)
+            else:
+                res = self.oerp.db.create_and_wait(
+                    ARGS.super_admin_passwd,
+                    ARGS.database,
+                    False,
+                    'en_US',
+                    ARGS.passwd)
+                self.assertIsInstance(res, list)
+                self.assertNotEqual(res, list())
+                self.assertEqual(res[0]['login'], 'admin')
+                self.assertEqual(res[0]['password'], ARGS.passwd)
+            import time
+            time.sleep(10)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
